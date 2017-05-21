7d3399ef144182ea90747e46de8dfed376ec48f7
Victor Zhong <victor@victorzhong.com>
2017-05-21 15:13:31 -0700

    bump version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='vocab',
-    version='0.0.1',
+    version='0.0.2',
 
     description='Vocabulary management for NLP in Python.',
     long_description=long_description,
